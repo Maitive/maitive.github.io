a3d0ef6b00aadd3994c81e6b7257267e944b768e
alpatanecmetin <necmettin.cimen@alpata.com>
2022-06-01 19:59:46 +0300

    fix

--- js/maps2.js
+++ js/maps2.js
@@ -2,10 +2,7 @@ setTimeout(()=>{
     if(document.getElementById('map')){
         var map = L.map('map').setView([39.799905, 29.105530], 15);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 18,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1
+            maxZoom: 18
         }).addTo(map);
         L.marker([40.999905, 29.105530]).addTo(map)
             .bindPopup("Batı, Nida Kule, barbaros mahallesi, Begonya Sk. no:1,34746 Ataşehir/İstanbul").openPopup();
